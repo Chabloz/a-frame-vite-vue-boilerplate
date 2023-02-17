d6e2177adf92401dbd8fcef6a9168e7d86f4aa80
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2023-02-17 17:03:29 +0100

    Add licences and link to the onboarding

--- src/components/TheOnboarding.vue
+++ src/components/TheOnboarding.vue
@@ -11,20 +11,57 @@
 <template>
   <div id="onboarding" v-if="showOnboarding">
     <div>
-      <h1>A-Frame + Vue Boilerplate</h1>
+      <h1>A-Frame + Vite + Vue Boilerplate</h1>
       <p v-if="!loaded">loading...</p>
       <button v-if="loaded" @click="showOnboarding = false">Enter scene</button>
+      <div class="licences">
+        <p>Included in the boilerplate:</p>
+        <dl>
+          <dt>aframe-extras controls and animation-mixer</dt>
+          <dd>
+            <a href="https://github.com/c-frame/aframe-extras" target="_blank">https://github.com/c-frame/aframe-extras</a>
+             - <a href="https://github.com/c-frame/aframe-extras/blob/master/LICENSE" target="_blank">MIT License</a>
+          </dd>
+          <dt>aframe-blink-controls</dt>
+          <dd>
+            <a href="https://github.com/jure/aframe-blink-controls/" target="_blank">aframe-blink-controls</a>
+             - <a href="https://github.com/jure/aframe-blink-controls/blob/main/LICENSE" target="_blank">MIT License</a>
+          </dd>
+          <dt>simple-navmesh-constraint</dt>
+          <dd>
+            A component from <a href=" https://github.com/AdaRoseCannon/aframe-xr-boilerplate" target="_blank">aframe-xr-boilerplate</a> By Ada Rose Cannon
+            under MIT License
+          </dd>
+          <dt>3D Room model: VR exhibition gallery baked</dt>
+          <dd>
+            <a href="https://sketchfab.com/3d-models/vr-exhibition-gallery-baked-ee6f3b0d9db14b62a1b3aeef04315313" target="_blank">VR exhibition gallery baked</a>
+            by <a href="https://sketchfab.com/ida61xq" target="_blank">ChristyHsu</a>
+            is licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
+          </dd>
+        </dl>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+  a {
+    color: white;
+  }
+  .licences {
+    margin: 2rem 0;
+    font-size: 1rem;
+  }
+  .licences dt {
+    padding-top: 1rem;
+    font-weight: bold;
+  }
   #onboarding {
     position: absolute;
     top: 0;
     left: 0;
-    background-color: black;
-    color: white;
+    background-color: #333;
+    color: #ccc;
     width: 100%;
     height: 100%;
     padding: 1rem;
