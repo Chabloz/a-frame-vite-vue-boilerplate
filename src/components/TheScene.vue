50bd54dadd3d134a3d27db068110a44b8f7c6d4a
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2023-02-28 11:37:24 +0100

    test sphere stars systems

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, watch } from 'vue';
 
   import TheCameraRig from './TheCameraRig.vue';
   import TheNavMesh from './TheNavMesh.vue';
@@ -15,6 +15,44 @@
   });
 
   const allAssetsLoaded = ref(false);
+
+  // const angleAzimuth = Math.random() * 2 * Math.PI;
+  // const angleElevation = Math.random() * Math.PI/4;
+  // const vectNormal = new THREE.Vector3(1,1,1);
+  // const quat1 = new THREE.Quaternion();
+  // quat1.setFromEuler(new THREE.Euler(0 , angleAzimuth, 0));
+  // const quat2 = new THREE.Quaternion();
+  // quat2.setFromEuler(new THREE.Euler(angleElevation, 0, 0));
+  // quat1.multiply(quat2);
+  // vectNormal.applyQuaternion(quat1);
+  // console.log(vectNormal);
+  // vectNormal.multiplyScalar(10);
+  // console.log(vectNormal);
+
+  const points = (new THREE.SphereGeometry(10,15,30,0,Math.PI)).attributes.position.array;
+  const vectorsOnSphere = new Map();
+  for (let i = 0; i < points.length; i += 3) {
+    vectorsOnSphere.set(`${points[i]}-${points[i+1]}-${points[i+2]}`, new THREE.Vector3(points[i], points[i+1], points[i+2]));
+  }
+  const randomPoints = [...vectorsOnSphere.values()];
+  console.log(randomPoints);
+  // shuffle the random points with Fisher-Yates
+  for (let i = randomPoints.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [randomPoints[i], randomPoints[j]] = [randomPoints[j], randomPoints[i]];
+  }
+  watch(allAssetsLoaded, (newVal) => {
+    if (newVal) {
+      for (let i=0; i<40; i++) {
+        const vect3 = randomPoints[i];
+        const sphere = document.createElement('a-sphere');
+        sphere.setAttribute('position', `${vect3.x} ${vect3.y} ${vect3.z}`);
+        sphere.setAttribute('radius', '1');
+        sphere.setAttribute('color', 'red');
+        document.querySelector('#stars').appendChild(sphere);
+      }
+    }
+  });
 </script>
 
 <template>
@@ -26,10 +64,9 @@
       referenceSpaceType: local-floor;
       optionalFeatures: dom-overlay;
       overlayElement: ${overlaySelector};
-
-      ougwqdougqwd
     `"
   >
+    <a-entity id="stars" rotation="-90 0 0"></a-entity>
 
     <a-assets @loaded="allAssetsLoaded = true">
       <!--
