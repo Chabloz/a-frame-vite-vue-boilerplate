48b61c3fdb5a797d4eb3bb1bc5da47b8854a4a9d
jfavlam <jfavlam@pm.me>
2024-02-14 14:01:37 +0100

    feat: Add basic ssl to expose Vite server in HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 
 const fullReloadPlugin = {
@@ -12,6 +13,7 @@ export default defineConfig(({ command, mode }) => {
   const config = {
     base: "/aframe-vue-boilerplate/",
     plugins: [
+      basicSsl(),
       vue({
         template: {
           compilerOptions: {
