cc49dbae863fe2276781b581576bc128f7e0c105
jfavlam <jfavlam@pm.me>
2024-02-14 08:56:37 +0100

    build: Add @ alias to point /src in imports

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 const fullReloadPlugin = {
   handleHotUpdate({ server }) {
-    server.ws.send({type: 'full-reload'});
+    server.ws.send({ type: "full-reload" });
     return [];
   },
 };
 
-
-export default defineConfig(({command, mode}) => {
-
+export default defineConfig(({ command, mode }) => {
   const config = {
-    base: '/aframe-vue-boilerplate/',
-    plugins: [vue({
-      template: {
-        compilerOptions: {
-          // Allow A-Frame elements to be in Vue template
-          isCustomElement: tag => tag.startsWith('a-')
+    base: "/aframe-vue-boilerplate/",
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            // Allow A-Frame elements to be in Vue template
+            isCustomElement: (tag) => tag.startsWith("a-"),
+          },
         },
+      }),
+      fullReloadPlugin,
+    ],
+    resolve: {
+      alias: {
+        "@": "/src",
       },
-    }), fullReloadPlugin],
+    },
   };
 
   return config;
