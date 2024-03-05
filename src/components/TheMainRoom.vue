7a85f2526be907f23481b99d8d0f0726b1c236b8
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2024-03-05 16:04:00 +0100

    Add a sakura circle shader effect

--- src/components/TheMainRoom.vue
+++ src/components/TheMainRoom.vue
@@ -7,6 +7,8 @@
   import ExitDoor from './ExitDoor.vue';
 
   import '../aframe/listen-to.js';
+  import '../aframe/life-like-automaton.js';
+  import '../aframe/sakura-circle.js';
 
   defineProps({
     scale: Number,
@@ -31,7 +33,8 @@
       geometry="primitive: plane; height: 2; width: 2;"
       position="2 2 3.9"
       rotation="-180 0 0"
-      life-like-automaton="resolution: 64; maxGen: 30;  birthRule: 3,4,5,6,7; survivalRule: 5,6;"
+      alife-like-automaton="resolution: 64; maxGen: 30;  birthRule: 3,4,5,6,7; survivalRule: 5,6;"
+      sakura-circle="resolution: 64;"
     ></a-entity>
 
     <a-entity
