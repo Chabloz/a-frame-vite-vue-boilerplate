5528ada55c93138b16ddb11e7fe99e59b497ba10
Nicolas Chabloz <synn.void@gmail.com>
2024-10-29 15:26:35 +0100

    bugfix z value

--- src/components/TheLifeCubeRoom.vue
+++ src/components/TheLifeCubeRoom.vue
@@ -14,7 +14,7 @@
       material="src: #room-gol-out-texture"
       position="0 -0.5 1.9"
       rotation="0 180 0"
-      z="1"
+      :z="1"
       :rot="0"
       :cameraEffect="true"
       :cameraY="1.65"
