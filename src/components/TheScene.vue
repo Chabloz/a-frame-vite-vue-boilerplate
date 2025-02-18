dd9edac75cc7a5975e074f291ec7e3795f305327
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-02-18 13:54:42 +0100

    example collision

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -3,13 +3,22 @@
 
   import TheCameraRig from './TheCameraRig.vue';
   import '../aframe/tesselation-square.js';
+  import '../aframe/emit-when-near.js';
+  import '../aframe/event-set.js';
+  import '../aframe/listen-to.js';
+  import '../aframe/simple-grab.js';
+  import '../aframe/clickable.js';
 
   const allAssetsLoaded = ref(false);
+
+  function collision(event) {
+    console.log('collision', event.detail);
+  }
+
 </script>
 
 <template>
   <a-scene
-    stats
     fog="type: linear; color: #a3d0ed; near: 30; far: 60"
     background="color: #a3d0ed;"
   >
@@ -19,25 +28,46 @@
     </a-assets>
 
     <template v-if="allAssetsLoaded">
-      <a-entity light="type: ambient; color: #445451"></a-entity>
-      <a-entity light="type: point; intensity: 2" position="0 2 -10"></a-entity>
 
-      <a-ocean
-        width="100"
-        depth="100"
-        amplitude="0.2"
-        density="100"
-        amplitudeVariance="0.1"
-      ></a-ocean>
-      <a-ocean
-        width="100"
-        depth="100"
-        amplitude="0.05"
-        density="100"
-        amplitudeVariance="0.1"
-      ></a-ocean>
+      <a-sphere
+        id="sphere-box-1"
+        position="0 0 -5"
+        emit-when-near="distance: 2"
+        color="blue"
+        @click="collision($event)"
+        event-set__change-when-near="attribute: color; value: red"
+        event-set__change-when-far="event: unclick; attribute: color; value: blue"
+      ></a-sphere>
 
-      <a-entity tesselation-square="width: 10; depth: 10"></a-entity>
+      <a-sphere
+        position="10 0 -5"
+        color="green"
+        listen-to="target: #sphere-box-1;"
+        event-set="attribute: color; value: red"
+      ></a-sphere>
+
+      <a-box
+        obb-collider
+        id="radio"
+        position="-2 0 -3"
+        scale="2 2 2"
+        color="red"
+        visible="true"
+        event-set="event: obbcollisionstarted; attribute: visible; value: false"
+      ></a-box>
+
+      <a-box
+        obb-collider
+        id="power"
+        position="2 0 -3"
+        scale="0.2 0.2 0.2"
+        color="green"
+        simple-grab
+        clickable
+      ></a-box>
+
+      <a-plane position="0 0 -5" rotation="-90 0 0" width="100" height="100" color="#7bc8a4"></a-plane>
+
     </template>
 
     <TheCameraRig />
