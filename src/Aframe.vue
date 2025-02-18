771dbaf74081bd04b86543310dcfdd35e97cae80
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-02-18 09:40:30 +0100

    test ocean

--- src/Aframe.vue
+++ src/Aframe.vue
@@ -11,13 +11,5 @@
 <template>
   <TheOnboarding :loaded="loaded" />
 
-  <!-- The DOM element of the overlay must be mounted before the A-Frame Scene is mounted -->
-  <!-- Otherwise the "webxr system" of the A-Frame scene wont find the DOM Element -->
-  <TheOverlay v-model="scale" id="overlay" />
-
-  <TheScene
-    :scale="scale"
-    overlay-selector="#overlay"
-    @loaded="loaded = true"
-  />
+  <TheScene @loaded="loaded = true" />
 </template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -2,64 +2,42 @@
   import { ref } from 'vue';
 
   import TheCameraRig from './TheCameraRig.vue';
-  import TheMainRoom from './TheMainRoom.vue';
-  import TheLifeCubeRoom from './TheLifeCubeRoom.vue';
-  import ThePhysicRoom from './ThePhysicRoom.vue';
-
-  import '../aframe/simple-grab.js';
-
-  defineProps({
-    scale: Number,
-    overlaySelector: String,
-  });
+  import '../aframe/tesselation-square.js';
 
   const allAssetsLoaded = ref(false);
 </script>
 
 <template>
   <a-scene
-    background="color: black;"
-    :webxr="`
-      requiredFeatures: local-floor;
-      referenceSpaceType: local-floor;
-      optionalFeatures: dom-overlay;
-      overlayElement: ${overlaySelector};
-    `"
-    xr-mode-ui="XRMode: xr"
-    physx="
-      autoLoad: true;
-      delay: 1000;
-      useDefaultScene: false;
-      wasmUrl: lib/physx.release.wasm;
-    "
-    simple-grab
+    stats
+    fog="type: linear; color: #a3d0ed; near: 30; far: 60"
+    background="color: #a3d0ed;"
   >
 
     <a-assets @loaded="allAssetsLoaded = true">
-      <!--
-        Title: VR Gallery
-        Model source: https://sketchfab.com/3d-models/vr-gallery-1ac32ed62fdf424498acc146fad31f7e
-        Model author: https://sketchfab.com/mvrc.art (Maxim Mavrichev)
-        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
-      -->
-      <a-asset-item id="room" src="assets/vr_gallery.glb"></a-asset-item>
-      <!--
-        Title: 3D Gallery for VR projects
-        Model source: https://sketchfab.com/3d-models/3d-gallery-for-vr-projects-68f77ed8558c4bd59e0a13e2cc9d1fd1
-        Model author: https://sketchfab.com/tekuto1s (tekuto1s)
-        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
-      -->
-      <a-asset-item id="physic-room" src="assets/3d_gallery_for_vr_projects.glb"></a-asset-item>
-      <a-asset-item id="sound-1" response-type="arraybuffer" src="assets/sound1.mp3" preload="auto"></a-asset-item>
-      <img id="room-physic-out-texture" :src="`assets/main-room-from-physic-room.png`">
-      <img id="room-gol-out-texture" :src="`assets/main-room-from-gol-room.png`">
-      <img id="room-physic-texture" :src="`assets/physicRoom.png`">
+
     </a-assets>
 
     <template v-if="allAssetsLoaded">
-      <TheMainRoom :scale="scale" />
-      <TheLifeCubeRoom />
-      <ThePhysicRoom />
+      <a-entity light="type: ambient; color: #445451"></a-entity>
+      <a-entity light="type: point; intensity: 2" position="0 2 -10"></a-entity>
+
+      <a-ocean
+        width="100"
+        depth="100"
+        amplitude="0.2"
+        density="100"
+        amplitudeVariance="0.1"
+      ></a-ocean>
+      <a-ocean
+        width="100"
+        depth="100"
+        amplitude="0.05"
+        density="100"
+        amplitudeVariance="0.1"
+      ></a-ocean>
+
+      <a-entity tesselation-square="width: 10; depth: 10"></a-entity>
     </template>
 
     <TheCameraRig />
